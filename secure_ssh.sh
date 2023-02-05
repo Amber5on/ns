0dd8702cdf661995b1a1b370aa7e970bba00f646
Amber5on <121375278+Amber5on@users.noreply.github.com>
2023-02-05 19:51:21 +0200

    Update secure_ssh.sh

--- secure_ssh.sh
+++ secure_ssh.sh
@@ -2,6 +2,8 @@
 
 /bin/sed -i "s/#Port 22/Port 1111/g" /etc/ssh/sshd_config
 /bin/sed -i "s/#Protocol 2/Protocol 2/g" /etc/ssh/sshd_config
-/bin/sed -i "s/#UseDNS yes/UseDNS no/g" /etc/ssh/sshd_config
+/bin/sed -i "s/#UseDNS no/UseDNS no/g" /etc/ssh/sshd_config
+/bin/sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+/bin/sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 
 service sshd restart
